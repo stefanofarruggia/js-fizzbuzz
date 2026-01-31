2fa381ea2cc900295ba89a11ed6b81c73ab98961
stefanofarruggia <stefano.farruggia@icloud.com>
2026-01-31 18:27:37 +0100

    fix-comments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,19 +8,18 @@
 
 //mettiamo un for per fargli stampare tutti i numeri da 1 a 100
 
-for (let i = 0; i <= 100; i++) {
-    //mettiamo un if + console.log perstampare come FizzBuzz i multipli sia di 3 che di 5
-    //lo mettiamo per primo per far si che non venga sovrascritto
+for (let i = 0; i <= 100; i++) {        //mettiamo un if + console.log perstampare come FizzBuzz i multipli sia di 3 che di 5
+                                        //lo mettiamo per primo per far si che non venga sovrascritto
     if (i % 3 == 0 && i % 5 == 0){
         console.log("FizzBuzz")
-    //mettiamo un if + console.log per fargli stampare come Fizz tutti i multipli di 3 
-    } else if (i % 3 == 0) {
+     
+    } else if (i % 3 == 0) {            //mettiamo un if + console.log per fargli stampare come Fizz tutti i multipli di 3
         console.log("Fizz")
-    //mettiamo un if + console.log per fargli stampare come Buzz tutti i multipli di 5
-    } else if (i % 5 == 0){
+    
+    } else if (i % 5 == 0){             //mettiamo un if + console.log per fargli stampare come Buzz tutti i multipli di 5
         console.log("Buzz")
-    //mettiamo un else per fargli stampare tutti gli altri numeri    
-    } else {
+        
+    } else {                            //mettiamo un else per fargli stampare tutti gli altri numeri
         console.log(i)
     }
 }
